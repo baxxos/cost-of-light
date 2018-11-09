d928c82d3fb81a18c56fa628225fa36ebbcb09e0
Michal Meluch <meluch.m@gmail.com>
2018-11-09 10:09:16 +0100

    Improved lightning effects

---
+++ Assets/LampFlickerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LampFlickerController : MonoBehaviour {
+    [Tooltip("Sprite which the flickering effect should follow")]
+    public SpriteRenderer spriteToFollow;
+    [Tooltip("Sprite bone to which the flickering effect should adjust during animations")]
+    public GameObject boneToFollow;
+    [Tooltip("X offset from the bone to follow")]
+    public float yOffset;
+    [Tooltip("Y offset from the bone to follow")]
+    public float xOffset;
+    private SpriteRenderer spriteRenderer;
+    private bool flippedLeft = false; 
+    
+	// Use this for initialization
+	void Start () {
+        this.spriteRenderer = GetComponent<SpriteRenderer>();
+        this.transform.position.Set(100, 100, 100);
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (flippedLeft && Input.GetKeyDown("d"))
+        {
+            this.flippedLeft = false;
+        }
+        else if (!flippedLeft && Input.GetKeyDown("a"))
+        {
+            this.flippedLeft = true;
+        }
+
+        // TODO: refactor this mess
+        if (flippedLeft)
+        {
+            this.transform.position = new Vector3(
+                2 * spriteToFollow.transform.position.x - boneToFollow.transform.position.x - xOffset, 
+                boneToFollow.transform.position.y + yOffset, 
+                boneToFollow.transform.position.z
+            );
+        }
+        else
+        {
+            this.transform.position = new Vector3(
+                boneToFollow.transform.position.x + xOffset, 
+                boneToFollow.transform.position.y + yOffset,
+                boneToFollow.transform.position.z
+            );
+        }
+    }
+}
---
+++ Assets/LanternController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LanternController : MonoBehaviour {
+    public bool enabledByDefault;
+    private SpriteRenderer spriteRenderer;
+    private GameObject[] enlightedSprites;
+    private List<Color> enlightedSpritesColors;
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(true);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.enabled = enabledByDefault;
+
+        enlightedSprites = GameObject.FindGameObjectsWithTag("Player");
+        enlightedSpritesColors = new List<Color>(enlightedSprites.Length);
+
+        if (!enabledByDefault)
+        {
+            foreach (GameObject sprite in enlightedSprites)
+            {
+                enlightedSpritesColors.Add(sprite.GetComponent<SpriteRenderer>().color);
+                sprite.GetComponent<SpriteRenderer>().color = Color.black;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(1) || Input.GetKeyDown("e"))
+        {
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+
+            // TODO: refactor this mess
+            int index = 0;
+            foreach (GameObject sprite in enlightedSprites)
+            {
+                sprite.GetComponent<SpriteRenderer>().color = (spriteRenderer.enabled ? enlightedSpritesColors[index]: Color.black);
+                index++;
+            }
+        }
+    }
+}
