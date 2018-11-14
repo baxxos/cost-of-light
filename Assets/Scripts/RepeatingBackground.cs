01ff4dc7855f62f021068e097315dc69df06d2c3
Michal Meluch <meluch.m@gmail.com>
2018-11-14 22:28:05 +0100

    Implemented repeating parallax background

---
+++ Assets/Scripts/RepeatingBackground.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatingBackground : MonoBehaviour {
+    public Camera cameraToFollow;
+    private float cameraExtentHorizontal;
+    private float cameraExtentVertical;
+    private float width;
+    private float height;
+
+    private void AdjustPositionToRight()
+    {
+        transform.position = new Vector2(
+            transform.position.x + 3 * this.width, 
+            transform.position.y
+        );
+    }
+
+    private void AdjustPositionToLeft()
+    {
+        transform.position = new Vector2(
+            transform.position.x - 3 * this.width,
+            transform.position.y
+        );
+    }
+
+    private float GetRightCameraEdge()
+    {
+        return cameraToFollow.transform.position.x + (this.cameraExtentHorizontal);
+    }
+
+    private float GetLeftCameraEdge()
+    {
+        return cameraToFollow.transform.position.x - (this.cameraExtentHorizontal);
+    }
+
+    // Use this for initialization
+    void Start () {
+        this.width = GetComponent<SpriteRenderer>().bounds.size.x;
+        this.height = GetComponent<BoxCollider2D>().size.y;
+        this.cameraExtentVertical = cameraToFollow.orthographicSize;
+        this.cameraExtentHorizontal = this.cameraExtentVertical * Screen.width / Screen.height;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (GetRightCameraEdge() >= transform.position.x + 2.5 * this.width)
+        {
+            AdjustPositionToRight();
+        }
+        else if (GetLeftCameraEdge() <= transform.position.x - 2.5 * this.width)
+        {
+            AdjustPositionToLeft();
+        }
+	}
+}
---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+    [Tooltip("Game object whose RigidBody2D will be used as a reference for scrolling.")]
+    public GameObject objectToFollow;
+    [Tooltip("Scrolling speed (0 for no scrolling and 1 for natural scrolling)")]
+    public float scrollSpeed;
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        this.rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Keep only a fixed portion of the velocity of the followed object
+        Vector2 referenceVelocity = objectToFollow.GetComponent<Rigidbody2D>().velocity;
+        this.rb2d.velocity = referenceVelocity - (referenceVelocity * scrollSpeed);
+	}
+}
