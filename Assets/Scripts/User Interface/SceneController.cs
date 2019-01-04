ec964200804fe0f3f46438cbccf4d98a0981ddf0
Michal Meluch <meluch.m@gmail.com>
2019-01-05 00:18:03 +0100

    Added new scene & pause screen

---
+++ Assets/Scripts/User Interface/SceneController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneController : MonoBehaviour {
+    public GameObject pausePanel;
+
+    private bool isScenePaused = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            TogglePause();
+        }
+	}
+
+    private void TogglePause()
+    {
+        Time.timeScale = (isScenePaused ? 1 : 0);
+        isScenePaused = !isScenePaused;
+        pausePanel.SetActive(isScenePaused);
+    }
+
+    public void ResumeScene()
+    {
+        Time.timeScale = 1;
+        isScenePaused = false;
+        pausePanel.SetActive(false);
+    }
+
+    public void PauseScene()
+    {
+        Time.timeScale = 0;
+        isScenePaused = true;
+        pausePanel.SetActive(true);
+    }
+
+    public void ExitScene()
+    {
+
+    }
+}
