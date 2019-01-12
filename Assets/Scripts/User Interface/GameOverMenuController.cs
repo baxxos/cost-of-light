7dc00fb14f9fad2c5d236855bc718b9c193059ef
Michal Meluch <meluch.m@gmail.com>
2019-01-13 00:57:14 +0100

    Added game over screen

---
+++ Assets/Scripts/User Interface/GameOverMenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOverMenuController : MonoBehaviour {
+    public GameObject gameOverPanel;
+    public PlayerCombatController combatController;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnEnable ()
+    {
+        combatController.OnPlayerHealthZero += ShowGameOverMenu;
+    }
+
+    void OnDisable ()
+    {
+        combatController.OnPlayerHealthZero -= ShowGameOverMenu;
+    }
+
+    private void ShowGameOverMenu ()
+    {
+        gameOverPanel.SetActive(true);
+        Time.timeScale = 1 - 0;
+    }
+}
